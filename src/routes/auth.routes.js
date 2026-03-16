f7648f1c894f4c96d83b321fc975980264e2c832
Nayan <nayanbhusari02@gmail.com>
2026-03-16 14:02:52 +0530

    env modified

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,40 +1,27 @@
 import { Router } from "express";
+
 import {
   register,
   verifyEmail,
   login,
   getMe,
 } from "../controllers/auth.controller.js";
+
 import {
   registerValidator,
   loginValidator,
-} from "../validators/auth.validator.js";
-import { authUser } from "../middleware/auth.middleware.js";
+} from "../validator/auth.validator.js";
+
+import { authUser } from "../middlewares/auth.middleware.js";
 
 const authRouter = Router();
 
-/**
- * @route POST /api/auth/register
- * @desc Register a new user
- */
 authRouter.post("/register", registerValidator, register);
 
-/**
- * @route POST /api/auth/login
- * @desc Login user and return JWT token
- */
 authRouter.post("/login", loginValidator, login);
 
-/**
- * @route GET /api/auth/get-me
-  Get current logged in user's details
- * @access Private
- */
 authRouter.get("/get-me", authUser, getMe);
 
-/**
- * @route GET /api/auth/verify-email
- */
 authRouter.get("/verify-email", verifyEmail);
 
 export default authRouter;
