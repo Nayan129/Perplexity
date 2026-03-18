809328a84c6d861aff18e589ee75eafb67288780
Nayan <nayanbhusari02@gmail.com>
2026-03-18 07:55:12 +0530

    frontend integration start

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -6,8 +6,4 @@ const model = new ChatGoogleGenerativeAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
 
-export async function testAi() {
-  model.invoke("how do i crack any interview for frontend backend or full stack developer within just 10 days of prepration?").then((response) => {
-    console.log(response.text);
-  });
-}
+
