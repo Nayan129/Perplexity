94d499fcff1c832ea2fc069ddee48a38b868025e
Nayan <nayanbhusari02@gmail.com>
2026-03-23 09:01:40 +0530

    delete route

--- Backend/src/routes/chat.routes.js
+++ Backend/src/routes/chat.routes.js
@@ -3,6 +3,7 @@ import {
   sendMessage,
   getChats,
   getMessages,
+  deleteChat,
 } from "../controllers/chat.controller.js";
 import { authUser } from "../middlewares/auth.middleware.js";
 
@@ -13,5 +14,7 @@ chatRouter.post("/message", authUser, sendMessage);
 chatRouter.get("/", authUser, getChats);
 
 chatRouter.get("/:chatId/messages", authUser, getMessages);
+
+chatRouter.delete("/delete/:chatId/", authUser, deleteChat);
 
 export default chatRouter;
